714374c83f40825d8a9ecc1a55162f5cf4908afe
LohithMarneni <lohith.marneni@gmail.com>
2025-05-26 15:55:12 +0530

    changed the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 
 function Header() {
   return (
-    <nav className=" p-2 bg-white text-white w-full border-b-2 border-black bg-black shadow-md">
-      <h1 className="text-2xl font-bold font-serif text-center  text-white">Custom Event Calender</h1>
+    <nav className=" p-2 bg-white text-white w-full border-b-2 border-black shadow-md">
+      <h1 className="text-2xl font-bold font-serif text-center text-black">Custom Event Calender</h1>
     </nav>
   );
 }
